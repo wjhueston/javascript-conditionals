1bceea740200657db0c63313849ec45cfc985d5a
wjhueston <wjhueston@gmail.com>
2020-11-09 21:00:34 -0600

    Updated for Class 7 Lab

--- conditionals.js
+++ conditionals.js
@@ -1,83 +1,117 @@
-$(document).ready(function () {
-    // add the functions as event listeners
-    // to the forms in the HTML
+$(document).ready(
+    function () {
+    //Events
+    $("#clickCounter").submit(countClick)
+    $("#ageValidator").submit(checkAge)
+    $("#salesTax").submit(calcSalesTax)
+    $("#catFood").submit(recommendFood)
+    $("#randomCard").submit(drawCard)
 
-	function countClick() {
-		// Increment a variable that tracks the
-		// number of button clicks
-
-		// Print the current number of clicks to the
-		// <p> with ID "clickCountOutput"
-
-		// When the count gets to 10, reset it to 0
-
-
+    //Functions
+    var clicks = 0;
+	function countClick(event){
+        event.preventDefault()
+        if(clicks === 10) clicks = 0;
+	    clicks+=1
+		$("#clickCountOutput").text("Total clicks: " + clicks)
 
 	}
 
 
-    function checkAge() {
-        // Get the user's birth year from the text
-        // box with ID of "birthYear"
-
-        // If they are currently under 18, print "Child"
-        // to the <p> with ID of "birthYearOutput"
-
-        // If they are 18 or over, print "Adult" instead
-
+    function checkAge(event) {
+	    event.preventDefault()
+        var birthYear = $("#birthYear").val();
+	    var currentYear = new Date()
+        currentYear = currentYear.getFullYear()
+        var age = currentYear - birthYear
+        if(age < 18) $("#birthYearOutput").text("Child")
+        else $("#birthYearOutput").text("Adult")
     }
 
-    function calcSalesTax() {
-        // Get the purchase amount from the text
-        // box with ID of "purchaseAmount"
-
-        // Get the state from the text box with ID "state"
-
-        // Tax rates are: WI 5%, IL 8%, MN 7.5%, MI 5.5%
-
-        // Calculate the sales tax amount and print to
-        // the <p> with ID of "salesTaxOutput"
-
-        // If the user enters a state not listed above,
-        // print "Error" instead
-
-
+    function calcSalesTax(event) {
+	    event.preventDefault()
+        var purchaseAmount = $("#purchaseAmount").val();
+        var state = $("#state").val();
+        var taxAmount
+        //I prefer to use Switch statements for flow control where there are four or more possible outcomes.
+        //I'll include the if-elseif-else statement below, commented out.
+        switch(state){
+            case "WI":
+                taxAmount = "$" + (purchaseAmount * 0.05).toFixed(2)
+                break;
+            case "IL":
+                taxAmount = "$" + (purchaseAmount * 0.08).toFixed(2)
+                break;
+            case "MN":
+                taxAmount = "$" + (purchaseAmount * 0.075).toFixed(2)
+                break;
+            case "MI":
+                taxAmount = "$" + (purchaseAmount * 0.055).toFixed(2)
+                break;
+            default:
+                taxAmount = "Error"
+                break;
+        }
+        /*if(state ==="WI"){
+            taxAmount = "$" + (purchaseAmount * 0.05).toFixed(2);
+        }
+        else if(state==="IL"){
+            taxAmount = "$" + (purchaseAmount * 0.08).toFixed(2);
+        }
+        else if(state==="MN"){
+            taxAmount = "$" + (purchaseAmount * 0.075).toFixed(2);
+        }
+        else if(state==="MI"){
+            taxAmount = "$" + (purchaseAmount * 0.055).toFixed(2);
+        }
+        else{
+            taxAmount = "Error"
+        }*/
+        $("#salesTaxOutput").text(taxAmount)
     }
 
-    function recommendFood() {
-        // Get the cat's age from the text box with
-        // ID of "catAge"
-
-        // Cats under 2 get "kitten chow", between 2 and 10
-        // get "adult chow", and over 10 get "senior chow"
-
-        // Print the food recommendation to the <p> with
-        // ID of "catAgeOutput"
-
-
+    function recommendFood(event) {
+	    event.preventDefault()
+        var age = $("#catAge").val()
+        var recommendation
+        if(age < 2) recommendation = "Recommended: Kitten Chow"
+        else if(age >= 2 && age <= 10) recommendation = "Recommended: Adult Chow"
+        else recommendation = "Recomendado: Señor Chow"
+        $("#catAgeOutput").text(recommendation)
     }
 
-    function drawCard() {
+    function drawCard(event) {
+        event.preventDefault()
         // Generate a random card face value (1 - 13)
         var faceValue = Math.floor(Math.random() * 13) + 1;
 
         // Generate a random suit (1 - 4)
         var suit = Math.floor(Math.random() * 4) + 1;
-
+        var suitName
         // Create the description of the card, for example
         // "King of Spades" or "2 of Hearts"
         var description;
-
-        // For face values 2 - 10, you can just print the number
-        // Face value 1 is "Ace", 11 is "Jack", 12 is "Queen",
-        // and 13 is "King"
-
-        // For the suits, 1 is "Clubs", 2 is "Spades",
-        // 3 is "Hearts", 4 is "Diamonds"
-
-        // Print the card's description to the <p> with
-        // ID of "drawCardOutput"
-
+        switch(suit){
+            case 1:
+                suitName = "Clubs"
+                break;
+            case 2:
+                suitName = "Spades"
+                break;
+            case 3:
+                suitName = "Hearts"
+                break;
+            case 4:
+                suitName = "Diamonds"
+                break;
+            default: break;
+        }
+        if(faceValue === 1) faceValue = "Ace"
+        else if(faceValue === 11) faceValue = "Jack"
+        else if(faceValue === 12) faceValue = "Queen"
+        else if (faceValue === 13) faceValue = "King"
+        description = faceValue + " of " + suitName
+        $("#drawCardOutput").text("Your card: " + description)
 
     }
 
